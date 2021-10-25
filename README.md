c8cef0af65ffd02f672db72c431a8ee699d7728f
Ameni <kallel.ameni@gmail.com>
2021-10-25 17:25:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/igorcarmo/springboot-gitpod-template)
+
+# Spring Boot Gitpod Template
+
+Simple Spring Boot template repository for Gitpod usage, enabling repository bootstraping and development on web browser.
---
+++ src/main/java/com/igorac/springbootgitpodtemplate/SpringbootGitpodTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.igorac.springbootgitpodtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitpodTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitpodTemplateApplication.class, args);
+	}
+
+}
